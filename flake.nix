07bf0d90e7c098fbaee2fe20490ce823a5726803
Jessy <contact@moysus.com>
2026-06-22 10:46:46 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "aarch64-darwin";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          pkgs.nodejs_26
+        ];
+      };
+    };
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export class Yexception {
+  public static helloWorld(): string {
+    return "Hello World";
+  }
+}
